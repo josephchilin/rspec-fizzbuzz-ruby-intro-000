409404b15f656c7584338e8ac11794537f2df04d
amazing-callback-3381 <joseph@josephchilin.com>
2020-01-21 21:19:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  int int%3==
+  if int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0 
+    "Buzz"
+  
   
 end
